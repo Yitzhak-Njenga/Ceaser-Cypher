d0d8275908bd4fbdc0556ecdaa13322dbfa34ca6
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-22 18:11:13 +0300

    added a test on decrpttest

--- src/main/java/DecryptMe.java
+++ src/main/java/DecryptMe.java
@@ -3,4 +3,12 @@ public class DecryptMe {
     private static int shiftKey;
     private static String cipherText = "";
     private static String errorMessage = "";
+
+
+
+    public  String decodeMethod(String encryptedText, int shiftKey) {
+        this.encryptedText = encryptedText;
+        if (shiftKey < 1 || shiftKey > 26) {
+            return errorMessage = "key must be between 1 to 25";
+        }
 }
