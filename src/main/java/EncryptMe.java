aec249dc242e241ff8312109e2a98b8507968bed
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-22 17:38:38 +0300

    added logic for shift key

--- src/main/java/EncryptMe.java
+++ src/main/java/EncryptMe.java
@@ -3,4 +3,12 @@ public class EncryptMe {
     private static int shiftKey;
     private static String cipherText = "";
     private static String errorMessage = "";
-}
+
+
+    public  String encodeMethod(String text, int shiftKey) {
+        this.text = text;
+
+        if (shiftKey > 26 || shiftKey < 0) {
+            return errorMessage = "key must be between 1 to 25";
+        }
+    }
