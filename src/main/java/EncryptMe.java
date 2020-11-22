a3e14a28fc93bd77e9a7c5729e97bdbd7f4d54ed
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-22 17:53:23 +0300

    added logic for encrpt cipher

--- src/main/java/EncryptMe.java
+++ src/main/java/EncryptMe.java
@@ -5,10 +5,40 @@ public class EncryptMe {
     private static String errorMessage = "";
 
 
-    public  String encodeMethod(String text, int shiftKey) {
+    public String encodeMethod(String text, int shiftKey) {
         this.text = text;
 
         if (shiftKey > 26 || shiftKey < 0) {
             return errorMessage = "key must be between 1 to 25";
+        }else{
+            int length = text.length();
+            for (int i = 0; i < length; i++) {
+                char ch = text.charAt(i);
+                if (Character.isLetter(ch)) {
+                    if (Character.isUpperCase(ch)) {
+                        char c = (char) (ch + shiftKey);
+                        if (c > 'Z') {
+                            cipherText += (char) (ch - (26 - shiftKey));
+                        } else {
+                            cipherText += c;
+                        }
+                    } else if (Character.isLowerCase(ch)) {
+                        char c = (char) (ch + shiftKey);
+                        if (c > 'z') {
+                            cipherText += (char) (ch - (26 - shiftKey));
+                        } else {
+                            cipherText += c;
+                        }
+                    }
+                } else {
+                    cipherText += ch;
+
+                }
+            }
+
         }
+
+        return cipherText;
     }
+
+}
