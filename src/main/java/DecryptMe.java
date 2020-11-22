8b3eba1d7f779e872907ff9159f748d917110edf
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-22 17:32:52 +0300

    created decrpte variables

--- src/main/java/DecryptMe.java
+++ src/main/java/DecryptMe.java
@@ -1,3 +1,6 @@
 public class DecryptMe {
-
+    private static String text = "";
+    private static int shiftKey;
+    private static String cipherText = "";
+    private static String errorMessage = "";
 }
